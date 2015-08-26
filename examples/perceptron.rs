8a753f1d17cdd06def5cc5cbbd4ed94eeefe8c0d
Jason Ramapuram <jason.ramapuram@viasat.com>
2015-08-26 12:16:27 +0200

    cleanup data generator

--- examples/perceptron.rs
+++ examples/perceptron.rs
@@ -19,11 +19,15 @@ fn build_optimizer(name: &'static str) -> Result<Box<Optimizer>, HALError> {
 
 fn generate_sin_wave(input_dims: u64, num_rows: u64) -> Vec<Array> {
   let dims = Dim4::new(&[input_dims, 1, 1, 1]);
-  let mut range = af::range(dims, 0, af::Aftype::F32).unwrap();
+  let delta = 2.0*3.1415/(input_dims as f32 - 1.0);
+
+  let index = af::range(dims, 0, af::Aftype::F32).unwrap();
+  let mut range = af::mul(&index, &delta).unwrap();
   let mut waves = Vec::<Array>::new();
-  for _ in (0..num_rows) {
+  
+  for i in (input_dims..num_rows+input_dims) {
     waves.push(af::sin(&range).unwrap());
-    range = af::add(&(1.0 as f32), &range).unwrap();
+    range = af::mul(&af::add(&index, &(1.0 as f32)).unwrap(), &delta).unwrap();
   }
   waves
 }
@@ -50,7 +54,7 @@ fn main() {
 
   // Test with learning to predict sin wave
   let data = generate_sin_wave(input_dims, num_train_samples);
-
+  
   // iterate our model
   let (loss, prediction) = model.fit(&data, &data, batch_size, iter, true);
   println!("prediction: "); af::print(&prediction);
