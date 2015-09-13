86d64ea1725e05a8247d0fb2119d299dfb05f939
Jason Ramapuram <jason.ramapuram@viasat.com>
2015-09-13 12:37:32 +0200

    cleanup optimizer

--- src/optimizer/mod.rs
+++ src/optimizer/mod.rs
@@ -1,21 +1,21 @@
 pub use self::sgd::SGD;
 mod sgd;
 
+use af;
+use af::{Array};
 use std::collections::HashMap;
 
-use af::{Array};
+use loss;
+use activations;
 use layer::Layer;
 use error::HALError;
 
 pub trait Optimizer {
   fn new(params: &HashMap<&str, &str>) -> Self where Self: Sized;
-  fn grads(&self, prediction: &Array, target: &Array
-           , loss: &'static str, activation_type: &'static str) -> Array;
   fn optimize(&mut self, layers: &mut Vec<Box<Layer>>
               , prediction: &Array
               , target: &Array
               , loss: &'static str) -> f32;
-  fn update_delta(&self, layer: &mut Box<Layer>, prev_activation: &Array, diffs: &Array);
   fn update_parameters(&self, layers: &mut Vec<Box<Layer>>, batch_size: u64);
   fn info(&self);
 }
@@ -32,4 +32,19 @@ pub fn get_default_optimizer(name: &str) -> Result<Box<Optimizer>, HALError>{
     "sgd" => Ok(Box::new(SGD::default())),
     _     => Err(HALError::UNKNOWN),
   }
+}
+
+fn grads(prediction: &Array, target: &Array
+         , loss: &'static str, activation_type: &'static str) -> Array
+{
+  // d_L = d_loss * d(z) where z = activation w/out non-linearity
+  let d_loss = loss::get_loss_derivative(loss, prediction, target).unwrap();
+  let d_z = activations::get_activation_derivative(activation_type, prediction).unwrap();
+  af::mul(&d_loss, &d_z).unwrap()
+}
+
+fn update_delta(layer: &mut Box<Layer>, prev_activation: &Array, diffs: &Array)
+{
+  // delta = ( a_{l-1} * d_l, d_l )
+  layer.update(&af::matmul(diffs, prev_activation, af::MatProp::NONE, af::MatProp::TRANS).unwrap(), diffs);
 }
--- src/optimizer/sgd.rs
+++ src/optimizer/sgd.rs
@@ -2,7 +2,7 @@ use af;
 use loss;
 use activations;
 use layer::{Layer};
-use optimizer::Optimizer;
+use optimizer::{Optimizer, update_delta, grads};
 use af::{Array};
 use std::collections::HashMap;
 use std::default::Default;
@@ -44,15 +44,6 @@ impl Optimizer for SGD {
     }
   }
   
-  fn grads(&self, prediction: &Array, target: &Array
-           , loss: &'static str, activation_type: &'static str) -> Array
-  {
-    // d_L = d_loss * d(z) where z = activation w/out non-linearity
-    let d_loss = loss::get_loss_derivative(loss, prediction, target).unwrap();
-    let d_z = activations::get_activation_derivative(activation_type, prediction).unwrap();
-    af::mul(&d_loss, &d_z).unwrap()
-  }
-
   fn update_parameters(&self, layers: &mut Vec<Box<Layer>>, batch_size: u64)
   {
     let alpha = self.learning_rate / batch_size as f32;
@@ -66,7 +57,7 @@ impl Optimizer for SGD {
                                                , &af::mul(&alpha, &delta_w).unwrap()).unwrap()
                                       , weight_num);
       }
-      
+
       // b = b - lr * d_l
       let biases = layers[layer_num].get_bias();
       for bias_num in (0..biases.len()) {
@@ -77,13 +68,7 @@ impl Optimizer for SGD {
     }
   }
 
-  //TODO: Add nesterov & momentum
-  fn update_delta(&self, layer: &mut Box<Layer>, prev_activation: &Array, diffs: &Array)
-  {
-    // delta = lr * a_{l-1} * d_l
-    layer.update(&af::matmul(diffs, prev_activation, af::MatProp::NONE, af::MatProp::TRANS).unwrap(), diffs);
-  }
-  
+
   fn optimize(&mut self, layers: &mut Vec<Box<Layer>>
               , prediction: &Array
               , target: &Array
@@ -93,23 +78,23 @@ impl Optimizer for SGD {
     self.learning_rate *= 1.0 / (1.0 + self.decay * (self.iter as f32));
     let last_index = layers.len() - 1;
     let prev_activation = layers[last_index].get_input();
-    let diffs = self.grads(prediction
-                           , target
-                           , loss
-                           , layers[last_index].get_activation_type());
-    self.update_delta(&mut layers[last_index], &prev_activation, &diffs);
+    let diffs = grads(prediction
+                      , target
+                      , loss
+                      , layers[last_index].get_activation_type());
+    update_delta(&mut layers[last_index], &prev_activation, &diffs);
 
     for i in (1..last_index).rev() {
       // d_l = (W_{l+1}^T * d_{l+1}) .* derivative(z) where z = activation w/out non-linearity
       let prev_activation = layers[i].get_input();
       let grad = activations::get_activation_derivative(layers[i].get_activation_type(), &prev_activation).unwrap();
       let diffs = layers[i].backward(&diffs, &grad);
-      self.update_delta(&mut layers[i], &prev_activation, &diffs);
+      update_delta(&mut layers[i], &prev_activation, &diffs);
     }
 
     loss::get_loss(loss, prediction, target).unwrap()
   }
-  
+
   fn info(&self){
     println!("name:          {}", self.name);
     println!("learning_rate: {}", self.name);
