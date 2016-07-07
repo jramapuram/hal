4ed2e0ca517fac083eb37c7c7ac9b21382768271
Jason Ramapuram <jason.ramapuram@viasat.com>
2016-07-07 18:49:27 +0200

    softmax difference row wise

--- src/activations.rs
+++ src/activations.rs
@@ -26,7 +26,7 @@ pub fn sigmoid(x: &Array) -> Array {
 /// exp(x_i) / sum(exp(x))
 pub fn softmax(x: &Array) -> Array {
   // page 185 of http://www.deeplearningbook.org/contents/mlp.html
-  let z = x - af::max_all(x).0 as f32;
+  let z = af::sub(x, &af::max(x, 0), true);
   let exponentiated = af::exp(&z);
   let sum_epx_x = af::sum_all(&exponentiated).0 as f32;
   let sum_exp_x_vec = utils::constant(x.dims(), x.get_type(), sum_epx_x);
